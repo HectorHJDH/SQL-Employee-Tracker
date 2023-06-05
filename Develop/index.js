ddabe86d2f4b2d182d24a33d50c5d49ac46fbb11
HectorJosueDH <hectorjosue78@gmail.com>
2023-06-05 09:48:26 -0600

    First commit, added file structure

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// Import fs module
+const fs = require('fs');
+const express = require('express');
+
+// Import the connection object
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to the database before starting the Express.js server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
